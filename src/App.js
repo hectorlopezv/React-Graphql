98ca8de95ee2c6861d926aa9bbd948afd516cbb3
hector vinicio lopez molinares <hectorvmlopez@gmail.com>
2021-03-15 22:22:50 -0500

    adding new style componete stuff

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import ListCategory from './components/ListCategory'
 import ListPhotoCards from './components/ListPhotoCards'
 import Logo from './components/Logo'
 
-import { GlobalStyle } from './globalStyles'
+import { GlobalStyle } from './components/styles/globalStyles'
 const App = () => {
   return (
     <>
